7caa5f3bc62bdcf898e6826f3e9169c5b62ace76
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-17 23:32:34 -0500

    Test - FirstApp

--- 01-counter-app/test/FirstApp.test.tsx
+++ 01-counter-app/test/FirstApp.test.tsx
@@ -5,7 +5,20 @@ describe('Tests in <FirstApp />', () => {
 
   test('Should must match with the snapshot', () => {
     const title = 'Hello, I am a title'
-    render(<FirstApp title={title}/>)
+    const {container} = render(<FirstApp title={title}/>)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  test('Should show the title in a h1', () => {
+    const title = 'Hello, I am a title'
+    const {container, getByText} = render(<FirstApp title={title}/>)
+
+    expect(getByText(title)).toBeTruthy()
+
+    const h1 = container.querySelector('h1')
+    expect(h1.innerHTML).toBe(title)
+    expect(h1.innerHTML).toContain(title)
   })
 
 })
