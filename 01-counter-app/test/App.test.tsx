205c7ad3642a97045bd40aaefdc203dfaa52fc6e
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-19 17:48:03 -0500

    Test - App

--- 01-counter-app/test/App.test.tsx
+++ 01-counter-app/test/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import App from '../src/App'
 
 describe('Tests in <App />', () => {
@@ -7,7 +7,7 @@ describe('Tests in <App />', () => {
 
   test('should show <App /> correctly', () => {
 
-    const {container} = render(<App value={initialCounter}/>)
+    const { container } = render(<App value={initialCounter}/>)
     expect(container).toMatchSnapshot()
 
   })
@@ -16,7 +16,48 @@ describe('Tests in <App />', () => {
     render(<App value={initialCounter}/>)
 
     expect(screen.getByText(initialCounter)).toBeTruthy()
-    expect(screen.getByRole('heading', {level: 2}).innerHTML).toContain(`${initialCounter}`)
+    expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(`${initialCounter}`)
+
+  })
+
+  test('Should increment counter with button +1', () => {
+
+    render(<App value={initialCounter}/>)
+
+    const buttonIncrement = screen.getByRole('button', { name: '+1' })
+    fireEvent.click(buttonIncrement)
+
+    expect(screen.getByText(initialCounter + 1)).toBeTruthy()
+    expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(`${initialCounter + 1}`)
+
+  })
+
+  test('Should decrement counter with button -1', () => {
+
+    render(<App value={initialCounter}/>)
+
+    const buttonDecrement = screen.getByRole('button', { name: '-1' })
+    fireEvent.click(buttonDecrement)
+
+    expect(screen.getByText(initialCounter - 1)).toBeTruthy()
+    expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(`${initialCounter - 1}`)
+
+  })
+
+  test('Should reset counter with button Reset', () => {
+
+    render(<App value={initialCounter}/>)
+
+    const buttonIncrement = screen.getByRole('button', { name: '+1' })
+    fireEvent.click(buttonIncrement)
+    fireEvent.click(buttonIncrement)
+    fireEvent.click(buttonIncrement)
+
+    const buttonReset = screen.getByRole('button', { name: 'Reset' })
+    fireEvent.click(buttonReset)
+
+    expect(screen.getByText(initialCounter)).toBeTruthy()
+    expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(`${initialCounter}`)
 
   })
 
