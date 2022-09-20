ff6cab83508dd44189a38bfdcead94c4ca7af0a1
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-19 21:42:05 -0500

    GifApp - Add new category with useState Hook

--- 02-gif-expert-app/src/GifExpertApp.tsx
+++ 02-gif-expert-app/src/GifExpertApp.tsx
@@ -1,7 +1,34 @@
+import React, { useState } from 'react'
+
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState<string[]>(['One Punch'])
+
+  const handleAddCategory = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    event.preventDefault()
+
+    setCategories(prev => [...prev, 'HunterXHunter'])
+  }
+
   return (
     <>
-      <h1>Gif</h1>
+      <h1>GifApp</h1>
+
+      <button
+        onClick={handleAddCategory}
+        type="submit"
+      >
+        Agregar
+      </button>
+
+      <ol>
+
+        {
+          categories.map((category, index) => (
+            <li key={index}>{category}</li>
+          ))
+        }
+
+      </ol>
     </>
   )
 }
