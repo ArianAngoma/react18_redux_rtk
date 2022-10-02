9474e164d99f41e6e5069e28313bbe15668e97e8
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-10-01 19:30:57 -0500

    GifApp - Test in component GifGrid

--- 02-gif-expert-app/test/components/GifGrid.test.tsx
+++ 02-gif-expert-app/test/components/GifGrid.test.tsx
@@ -1,11 +1,20 @@
 import { render, screen } from '@testing-library/react'
 import { GifGrid } from '../../src/components'
+import useFetchGifs from '../../src/hooks/useFetchGifs'
+
+jest.mock('../../src/hooks/useFetchGifs')
+const useFetchGifsMock = jest.mocked(useFetchGifs)
 
 describe('Pruebas en <GifGrid />', () => {
 
   const category = 'One Punch'
 
   test('Debe de mostrar el loading correctamente', () => {
+
+    useFetchGifsMock.mockReturnValue({
+      gifs: [],
+      isLoading: true
+    })
 
     render(<GifGrid category={category}/>)
 
@@ -18,7 +27,29 @@ describe('Pruebas en <GifGrid />', () => {
 
   test('Debe de mostrar items cuando se cargan imágenes useFetchGifs', () => {
 
+    const gifs = [
+      {
+        id: 'ABC',
+        url: 'https://localhost/cualquier/cosa.jpg',
+        title: 'Cualquier cosa'
+      },
+      {
+        id: '123',
+        url: 'https://localhost/cualquier/cosa2.jpg',
+        title: 'Cualquier cosa2'
+      }
+    ]
 
+    useFetchGifsMock.mockReturnValue({
+      gifs,
+      isLoading: false
+    })
+
+    render(<GifGrid category={category}/>)
+
+    // screen.debug()
+
+    expect(screen.getAllByRole('img').length).toBe(gifs.length)
 
   })
 
