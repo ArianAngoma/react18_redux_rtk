283064db1233a19dd3f5f29f0472dccdd4d3c52e
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-10-08 21:27:52 -0500

    HookApp - MultipleHooks - useFetch

--- 03-hook-app/src/03-examples/MultipleCustomHooks.tsx
+++ 03-hook-app/src/03-examples/MultipleCustomHooks.tsx
@@ -1,7 +1,7 @@
 import useFetch from '../hooks/useFetch'
 import useFetchInternet from '../hooks/useFetchInternet'
 
-export interface ResponseApi {
+interface ResponseApi {
   quote_id: number;
   quote: string;
   author: string;
@@ -22,15 +22,34 @@ const MultipleCustomHooks = () => {
     isLoading
   } = useFetchInternet<ResponseApi[]>({ url: 'https://www.breakingbadapi.com/api/quotes/1' }) */
 
-  console.log({
-    data,
-    error,
-    isLoading
-  })
+  const { quote } = (!!data && data[0]) as ResponseApi
 
   return (
     <>
       <h1>Breaking Quotes</h1>
+
+      {
+
+        (isLoading) ? (
+          <div className="alert alert-info text-center">
+            Loading...
+          </div>
+        ) : (
+          <blockquote className="blockquote text-end">
+
+            <p className="mb-1">{quote}</p>
+
+            <footer className="blockquote-footer">Arian Angoma</footer>
+
+          </blockquote>
+        )
+
+      }
+
+      <button className="btn btn-primary">
+        Next quote
+      </button>
+
     </>
   )
 
