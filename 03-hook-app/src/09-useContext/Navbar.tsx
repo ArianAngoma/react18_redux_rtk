35969c9cc087043e4f3c6d1a84be2069d17be520
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-10-18 17:08:31 -0500

    HookApp - useContext - NavLink

--- 03-hook-app/src/09-useContext/Navbar.tsx
+++ 03-hook-app/src/09-useContext/Navbar.tsx
@@ -1,17 +1,62 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
   return (
-    <>
-      <Link to={
-        {
-          pathname: '/',
-        }
-      }>Home</Link>
-      <Link to={'/about'}>About</Link>
-      <Link to={'/login'}>Login</Link>
-    </>
+    <nav className="navbar navbar-expand-lg bg-light rounded-3">
+      <div className="container-fluid">
+
+        <Link
+          className="navbar-brand"
+          to={{
+            pathname: '/'
+          }}
+        >
+          useContext
+        </Link>
+
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+
+            <li className="nav-item">
+              <NavLink
+                className={
+                  ({isActive}) => `nav-link ${isActive ? 'active' : ''}`
+                }
+                to="/"
+                end
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                className={
+                  ({isActive}) => `nav-link ${isActive ? 'active' : ''}`
+                }
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+                <NavLink
+                    className={
+                        ({isActive}) => `nav-link ${isActive ? 'active' : ''}`
+                    }
+                    to="/login"
+                >
+                    Login
+                </NavLink>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 
 }
