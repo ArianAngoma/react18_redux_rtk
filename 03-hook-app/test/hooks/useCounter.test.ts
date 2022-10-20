22c069c91f9de20b97d431dc62704acc835bf2ad
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-10-19 21:39:14 -0500

    HookApp - Tests - useCounter

--- 03-hook-app/test/hooks/useCounter.test.ts
+++ 03-hook-app/test/hooks/useCounter.test.ts
@@ -6,7 +6,28 @@ describe('Pruebas en useCounter', () => {
   test('Debe de retornar valores por defecto', () => {
 
     const { result } = renderHook(() => useCounter())
-    console.log(result)
+
+    const {
+      counter,
+      handleIncrement,
+      handleDecrement,
+      handleReset
+    } = result.current
+
+    expect(counter).toBe(0)
+    expect(handleIncrement).toEqual(expect.any(Function))
+    expect(handleDecrement).toEqual(expect.any(Function))
+    expect(handleReset).toEqual(expect.any(Function))
+
+  })
+
+  test('Debe de tener el counter en 100', () => {
+
+    const { result } = renderHook(() => useCounter({ initialState: 100 }))
+
+    const { counter } = result.current
+
+    expect(counter).toBe(100)
 
   })
 
