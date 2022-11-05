11b1c07712dfef26bdbdc31535de4910d67764ab
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-11-05 13:58:06 -0500

    HookApp - Tests - TodoItem - functions

--- 03-hook-app/test/08-useReducer/TodoItem.test.tsx
+++ 03-hook-app/test/08-useReducer/TodoItem.test.tsx
@@ -1,5 +1,5 @@
 import { Todo } from '../../src/08-useReducer/todoReducer'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import TodoItem from '../../src/08-useReducer/TodoItem'
 
 describe('Pruebas en el componente <TodoItem/>', () => {
@@ -37,6 +37,67 @@ describe('Pruebas en el componente <TodoItem/>', () => {
 
     const buttonElement = screen.getByRole('button')
     expect(buttonElement.className).toBe('btn btn-danger')
+
+  })
+
+  test('Debe de mostrarse el Todo completado', () => {
+
+    const todo: Todo = {
+      id: 1,
+      description: 'Aprender React',
+      done: true
+    }
+
+    render(
+      <TodoItem
+        todo={todo}
+        onRemoveTodo={onRemoveTodoMock}
+        onToggleTodo={onToggleTodoMock}
+      />
+    )
+
+    const spanElement = screen.getByText(todo.description)
+    expect(spanElement.className).toBe('align-self-center text-decoration-line-through')
+    expect(spanElement.className).toContain('align-self-center')
+    expect(spanElement.className).toContain('text-decoration-line-through')
+
+  })
+
+  test('El span debe de llamar la función onToggleTodo', () => {
+
+    render(
+      <TodoItem
+        todo={todo}
+        onRemoveTodo={onRemoveTodoMock}
+        onToggleTodo={onToggleTodoMock}
+      />
+    )
+
+    const spanElement = screen.getByText(todo.description)
+    // spanElement.click()
+    fireEvent.click(spanElement)
+
+    expect(onToggleTodoMock).toHaveBeenCalledTimes(1)
+    expect(onToggleTodoMock).toHaveBeenCalledWith(todo.id)
+
+  })
+
+  test('El botón debe de llamar la función onRemoveTodo', () => {
+
+    render(
+      <TodoItem
+        todo={todo}
+        onRemoveTodo={onRemoveTodoMock}
+        onToggleTodo={onToggleTodoMock}
+      />
+    )
+
+    const buttonElement = screen.getByRole('button')
+    // buttonElement.click()
+    fireEvent.click(buttonElement)
+
+    expect(onRemoveTodoMock).toHaveBeenCalledTimes(1)
+    expect(onRemoveTodoMock).toHaveBeenCalledWith(todo.id)
 
   })
 
