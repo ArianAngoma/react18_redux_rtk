e397469c68f541655dd6a41ce3078012f0d4ba35
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-11-12 15:55:49 -0500

    HeroApp - Search Page styles

--- 04-heroes-spa/src/heroes/pages/SearchPage.tsx
+++ 04-heroes-spa/src/heroes/pages/SearchPage.tsx
@@ -1,8 +1,65 @@
 import { FC } from 'react'
+import { HeroCard } from '../components'
 
 const SearchPage: FC = () => {
   return (
-    <h1>Search Page</h1>
+    <>
+
+      <h1>Search</h1>
+      <hr/>
+
+      <div className="row">
+
+        <div className="col-5">
+
+          <h4>Searching</h4>
+          <hr/>
+
+          <form>
+
+            <input
+              type="text"
+              placeholder="Search your hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+
+            <button className="btn btn-outline-primary mt-1">
+              Search...
+            </button>
+
+          </form>
+
+        </div>
+
+        <div className="col-7">
+
+          <h4>Results</h4>
+          <hr/>
+
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger">
+            There is no a hero with <b>ABC</b>
+          </div>
+
+          {/* <HeroCard
+            id={}
+            superhero={}
+            publisher={}
+            alter_ego={}
+            first_appearance={}
+            characters={}
+          /> */}
+
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
--- 04-heroes-spa/src/ui/components/Navbar.tsx
+++ 04-heroes-spa/src/ui/components/Navbar.tsx
@@ -42,6 +42,15 @@ const Navbar: FC = () => {
               DC
             </NavLink>
 
+            <NavLink
+              className={
+                ({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`
+              }
+              to="/search"
+            >
+              Search
+            </NavLink>
+
           </div>
 
         </div>
