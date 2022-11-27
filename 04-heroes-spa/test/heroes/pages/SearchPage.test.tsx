ec4d771307b6dcb1546586090246538496152a26
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-11-26 21:42:42 -0500

    HeroApp - test - Search Page - error alert

--- 04-heroes-spa/test/heroes/pages/SearchPage.test.tsx
+++ 04-heroes-spa/test/heroes/pages/SearchPage.test.tsx
@@ -27,4 +27,57 @@ describe('Pruebas en <SearchPage />', () => {
 
   })
 
+  test('Debe de mostrar a Batman y el input con el valor del query string', () => {
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: '/search',
+          element: <SearchPage/>,
+        }
+      ],
+      {
+        initialEntries: ['/search?q=batman'],
+      }
+    )
+
+    render(<RouterProvider router={router}/>)
+
+    // screen.debug()
+
+    const input = screen.getByRole('textbox') as HTMLInputElement
+    expect(input.value).toBe('batman')
+
+    const image = screen.getByRole('img') as HTMLImageElement
+    expect(image.src).toContain('/assets/dc-batman.jpg')
+
+  })
+
+  test('Debe de mostrar un error si no se encuentra el hero', () => {
+
+    const router = createMemoryRouter(
+      [
+        {
+          path: '/search',
+          element: <SearchPage/>,
+        }
+      ],
+      {
+        initialEntries: ['/search?q=error-hero'],
+      }
+    )
+
+    const { container } = render(<RouterProvider router={router}/>)
+
+    // screen.debug()
+
+    const input = screen.getByRole('textbox') as HTMLInputElement
+    expect(input.value).toBe('error-hero')
+
+    const alert = container.querySelector('.alert-danger')
+    expect(alert).toBeTruthy()
+    expect(alert?.textContent).toBe('There is no a hero with error-hero')
+
+  })
+
 })
