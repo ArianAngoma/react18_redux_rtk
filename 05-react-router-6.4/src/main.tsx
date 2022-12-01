5b93d28815081d2d9eda3c7298e0679a3e085739
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-11-30 22:46:08 -0500

    React Router V6.4 - action to edit contact

--- 05-react-router-6.4/src/main.tsx
+++ 05-react-router-6.4/src/main.tsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import ErrorPage from './error-page'
 import Contact, { loader as contactLoader } from './routes/contact'
-import EditContact from './routes/edit'
+import EditContact, { action as editAction } from './routes/edit'
 
 import './index.css'
 
@@ -25,7 +25,8 @@ const router = createBrowserRouter([
       {
         path: 'contacts/:contactId/edit',
         element: <EditContact/>,
-        loader: contactLoader
+        loader: contactLoader,
+        action: editAction
       }
     ]
   }
