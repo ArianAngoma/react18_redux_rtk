52d0d8b247905b136eda37ec74efe8b3d1124776
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-12-06 22:22:28 -0500

    React Router V6.4 - Patgless Routes

--- 05-react-router-6.4/src/main.tsx
+++ 05-react-router-6.4/src/main.tsx
@@ -15,31 +15,36 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Root/>,
-    errorElement: <ErrorPage/>,
     loader: rootLoader,
     action: rootAction,
+    errorElement: <ErrorPage/>,
     children: [
       {
-        index: true,
-        element: <Index/>
+        errorElement: <ErrorPage/>,
+        children: [
+          {
+            index: true,
+            element: <Index/>
+          },
+          {
+            path: 'contacts/:contactId',
+            element: <Contact/>,
+            loader: contactLoader,
+            action: contactAction
+          },
+          {
+            path: 'contacts/:contactId/edit',
+            element: <EditContact/>,
+            loader: contactLoader,
+            action: editAction
+          },
+          {
+            path: 'contacts/:contactId/destroy',
+            action: destroyAction,
+            errorElement: <div>Opps! There was an error.</div>
+          }
+        ]
       },
-      {
-        path: 'contacts/:contactId',
-        element: <Contact/>,
-        loader: contactLoader,
-        action: contactAction
-      },
-      {
-        path: 'contacts/:contactId/edit',
-        element: <EditContact/>,
-        loader: contactLoader,
-        action: editAction
-      },
-      {
-        path: 'contacts/:contactId/destroy',
-        action: destroyAction,
-        errorElement: <div>Opps! There was an error.</div>
-      }
     ]
   }
 ])
