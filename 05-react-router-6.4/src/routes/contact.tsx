df0a9da8d93f5b2e5109fd12c075e95a9ba5e2a5
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-12-06 22:14:56 -0500

    React Router V6.4 - Optimistic UI

--- 05-react-router-6.4/src/routes/contact.tsx
+++ 05-react-router-6.4/src/routes/contact.tsx
@@ -83,6 +83,10 @@ function Favorite ({ contact }: { contact: any }) {
   // yes, this is a `let` for later
   let favorite = contact.favorite
 
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get('favorite') === 'true'
+  }
+
   return (
     <fetcher.Form method="post">
       <button
