59edf49daf4d4b3c106384136a939c6ae2cac2aa
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-12-12 18:02:46 -0500

    Toolkit Redux - New Slice Posts

--- 08-redux-example-app/src/App.tsx
+++ 08-redux-example-app/src/App.tsx
@@ -1,9 +1,14 @@
 import Counter from './features/counter/Counter'
+import PostList from './features/posts/PostList'
 
 function App () {
   return (
     <main className="App">
-      <Counter/>
+
+      {/* <Counter/> */}
+
+      <PostList/>
+
     </main>
   )
 }
