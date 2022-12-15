2250db7bb5f04111126dc6c7e595fc3d105ded5e
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-12-15 12:28:04 -0500

    Toolkit Redux - PostExcerpt Component

---
+++ 08-redux-example-app/src/features/posts/PostExcerpt.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react'
+
+import { Post } from './postSlice'
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButtons from './ReactionButtons'
+
+interface PostExcerptProps {
+  post: Post
+}
+
+const PostExcerpt: FC<PostExcerptProps> = ({ post }) => {
+
+  return (
+    <article>
+
+      <h3>{post.title}</h3>
+      <p>{post.body.substring(0, 100)}</p>
+
+      <p className="postCredit">
+        <PostAuthor userId={post.userId}/>
+        <TimeAgo timestamp={post.date}/>
+      </p>
+
+      <ReactionButtons post={post}/>
+
+    </article>
+  )
+
+}
+
+export default PostExcerpt
--- 08-redux-example-app/src/features/posts/PostList.tsx
+++ 08-redux-example-app/src/features/posts/PostList.tsx
@@ -2,10 +2,8 @@ import { FC, useEffect } from 'react'
 
 import { useAppSelector } from '../hooks/useAppSelector'
 import { useAppDispatch } from '../hooks/useAppDispatch'
-import PostAuthor from './PostAuthor'
-import TimeAgo from './TimeAgo'
-import ReactionButtons from './ReactionButtons'
 import { fetchPosts } from './postSlice'
+import PostExcerpt from './PostExcerpt'
 
 const PostList: FC = () => {
 
@@ -27,19 +25,10 @@ const PostList: FC = () => {
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
   const renderedPosts = orderedPosts.map(post => (
-    <article key={post.id}>
-
-      <h3>{post.title}</h3>
-      <p>{post.body.substring(0, 100)}</p>
-
-      <p className="postCredit">
-        <PostAuthor userId={post.userId}/>
-        <TimeAgo timestamp={post.date}/>
-      </p>
-
-      <ReactionButtons post={post}/>
-
-    </article>
+    <PostExcerpt
+      key={post.id}
+      post={post}
+    />
   ))
 
   return (
