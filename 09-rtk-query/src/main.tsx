e0778b1aa5978e582cbc0a21e4a208c0521c1a15
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-12-26 12:36:19 -0500

    RTK Query Crud - createApi

--- 09-rtk-query/src/main.tsx
+++ 09-rtk-query/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react'
+
 import App from './App'
+import { apiSlice } from './api/apiSlice'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App/>
+    </ApiProvider>
   </React.StrictMode>,
 )
