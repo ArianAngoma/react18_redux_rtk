3596c9afdf6bf1a88be1bff984b01ad06a5111fa
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-12-30 16:17:47 -0500

    Redux Auth - baseQuery with re-authorization wrapper

---
+++ 10-redux-auth-example/src/app/api/apiSlice.ts
@@ -0,0 +1,50 @@
+import { BaseQueryApi, createApi, FetchArgs, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { RootState } from '@reduxjs/toolkit/dist/query/core/apiState'
+import { logOut, setCredentials } from '../../features/auth/authSlice'
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'http://localhost:3500',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token
+
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`)
+    }
+
+    return headers
+  }
+})
+
+const baseQueryWithReAuth = async (args: string | FetchArgs, api: BaseQueryApi, extraOptions: {}) => {
+  let result = await baseQuery(args, api, extraOptions)
+
+  if (result?.error?.status === 403) {
+
+    console.log('Re-authenticating...')
+
+    // Send refresh token to get new access token
+    const refreshTokenResult = await baseQuery('/refresh', api, extraOptions)
+    console.log('refreshTokenResult', refreshTokenResult)
+
+    if (refreshTokenResult.data) {
+      const user = (api.getState() as RootState).auth.user
+
+      // Store the new token
+
+      api.dispatch(setCredentials({
+        ...refreshTokenResult.data,
+        user,
+      }))
+
+      // Retry the original query with the new token
+      result = await baseQuery(args, api, extraOptions)
+
+    } else {
+      api.dispatch(logOut())
+    }
+
+  }
+
+  return result
+}
