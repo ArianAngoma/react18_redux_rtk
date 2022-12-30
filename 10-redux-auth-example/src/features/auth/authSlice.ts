436e3fc9d0eb3307596e39ed8a291251b5b21d88
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-12-30 15:41:01 -0500

    Redux Auth - authSlice

---
+++ 10-redux-auth-example/src/features/auth/authSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface AuthState {
+  user: any | null
+  token: string | null
+}
+
+const initialState: AuthState = {
+  user: null,
+  token: null
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+
+    setCredentials: (state, action: PayloadAction<AuthState>) => {
+      const {
+        user,
+        token
+      } = action.payload
+
+      state.user = user
+      state.token = token
+    },
+
+    logOut: (state) => {
+      state.user = null
+      state.token = null
+    }
+
+  }
+})
+
+export const {
+  setCredentials,
+  logOut
+} = authSlice.actions
+
+export default authSlice.reducer
+
+export const selectCurrentUser = (state: RootState) => state.auth.user
+
+export const selectCurrentToken = (state: RootState) => state.auth.token
