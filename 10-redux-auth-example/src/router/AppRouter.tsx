5b4bf3e2efa6f71718478fa30d52b575591c03fb
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-12-30 23:30:22 -0500

    Redux Auth - Routing

--- 10-redux-auth-example/src/router/AppRouter.tsx
+++ 10-redux-auth-example/src/router/AppRouter.tsx
@@ -1,11 +1,33 @@
 import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom'
 
 import App from '../App'
+import Public from '../components/Public'
+import Login from '../features/auth/Login'
+import RequiredAuth from '../features/auth/RequiredAuth'
+import Welcome from '../features/auth/Welcome'
 
 export const routerObject: RouteObject[] = [
   {
-    path: '/*',
-    element: <App/>
+    element: <App/>,
+    children: [
+      {
+        index: true,
+        element: <Public/>,
+      },
+      {
+        path: 'login',
+        element: <Login/>,
+      }
+    ]
+  },
+  {
+    element: <RequiredAuth/>,
+    children: [
+      {
+        path: 'welcome',
+        element: <Welcome/>,
+      }
+    ]
   }
 ]
 
