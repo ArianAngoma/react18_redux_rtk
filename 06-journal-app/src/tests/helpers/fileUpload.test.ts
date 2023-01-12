5d0cc360a288a690b2f2bc39b793c776b2ca768c
ArianAngoma <arian.angoma@tecsup.edu.pe>
2023-01-12 16:18:28 -0500

    Journal App - Test - Delete image Cloudinary

--- 06-journal-app/src/tests/helpers/fileUpload.test.ts
+++ 06-journal-app/src/tests/helpers/fileUpload.test.ts
@@ -1,4 +1,13 @@
+import { v2 as cloudinary } from 'cloudinary'
+
 import { fileUpload } from '../../helpers'
+
+cloudinary.config({
+  cloud_name: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
+  api_key: import.meta.env.VITE_CLOUDINARY_API_KEY,
+  api_secret: import.meta.env.VITE_CLOUDINARY_API_SECRET,
+  secure: true
+})
 
 describe('Pruebas en fileUpload', () => {
 
@@ -14,6 +23,23 @@ describe('Pruebas en fileUpload', () => {
 
     expect(typeof url).toBe('string')
     
+    const urlObject = new URL(url)
+    const segments = urlObject.pathname.split('/')
+
+    const imageId = segments[segments.length - 1].replace('.png', '')
+
+    await cloudinary.api.delete_resources(['journal/' + imageId], {
+      resource_type: 'image'
+    })
+
   })
-  
+
+  test('Debe de retornar un error', async () => {
+
+    const file = new File([], 'node.png')
+    
+    await expect(fileUpload(file)).rejects.toThrow('Error while uploading')
+
+  })
+
 })
