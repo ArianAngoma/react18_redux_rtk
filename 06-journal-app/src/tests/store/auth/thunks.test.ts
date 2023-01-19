7c6e7ac21e7d47ff385882e6ac93d72344a65e72
ArianAngoma <arian.angoma@tecsup.edu.pe>
2023-01-19 00:22:28 -0500

    Journal App - Test - New test

--- 06-journal-app/src/tests/store/auth/thunks.test.ts
+++ 06-journal-app/src/tests/store/auth/thunks.test.ts
@@ -1,5 +1,9 @@
+import {reject} from '@reduxjs/toolkit/src/'
+
 import * as providers from '../../../firebase/providers'
 import { clearStateLogout, startLoginWithEmailPassword, startLogout, store } from '../../../store'
+import { display } from '@mui/system'
+import { miniSerializeError } from '@reduxjs/toolkit'
 
 describe('Pruebas en los thunks de authSlice', () => {
 
@@ -28,7 +32,7 @@ describe('Pruebas en los thunks de authSlice', () => {
     
   })
 
-  test('Deberia dispararse startLoginWithEmailPassword - Fallido', async () => {
+  test('Deberia dispararse startLoginWithEmailPassword - Fallido 1', async () => {
 
     const user: providers.LoginWithEmailPasswordParams = {
       email: 'arian.angoma.js@gmail.com',
@@ -54,6 +58,60 @@ describe('Pruebas en los thunks de authSlice', () => {
 
     expect(state.auth.errorMessage).toBe('Email no encontrado')
     expect(state.auth.status).toBe('not-authenticated')
+
+  })
+
+  test('Deberia dispararse startLoginWithEmailPassword - Fallido 2', async () => {
+
+    const dispatchMock = vi.fn()
+
+    const user: providers.LoginWithEmailPasswordParams = {
+      email: 'arian.angoma.js@gmail.com',
+      password: '123123'
+    }
+
+    const response: providers.ResponseSignInRejected = {
+      ok: false,
+      errorMessage: 'Email no encontrado'
+    }
+
+    vi.spyOn(providers, 'loginWithEmailPassword').mockResolvedValue(response)
+
+    await startLoginWithEmailPassword(user)(dispatchMock, store.getState, null)
+
+    expect(providers.loginWithEmailPassword).toHaveBeenCalled()
+    expect(providers.loginWithEmailPassword).toHaveBeenCalledWith({
+      email: 'arian.angoma.js@gmail.com',
+      password: '123123'
+    })
+
+    const errorAction = dispatchMock.mock.calls[1][0]
+    
+    expect(errorAction.error).toEqual(miniSerializeError('Rejected'))
+    expect(errorAction.payload).toEqual(response.errorMessage)
+    expect(errorAction.meta.arg).toBe(user)
+
+  })
+
+  test('Deberia dispararse startLoginWithEmailPassword - Fallido 3', async () => {
+
+    const dispatchMock = vi.fn()
+
+    const user: providers.LoginWithEmailPasswordParams = {
+      email: 'arian.angoma.js@gmail.com',
+      password: '123123'
+    }
+
+    const response: providers.ResponseSignInRejected = {
+      ok: false,
+      errorMessage: 'Email no encontrado'
+    }
+
+    vi.spyOn(providers, 'loginWithEmailPassword').mockResolvedValue(response)
+
+    const unwrapPromise = startLoginWithEmailPassword(user)(dispatchMock, store.getState, {})
+
+    await expect(unwrapPromise.unwrap()).rejects.toBe(response.errorMessage)
 
   })
 
