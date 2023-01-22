27027d95b8c8378ca12b909bed5f0795af776068
ArianAngoma <arian.angoma@tecsup.edu.pe>
2023-01-22 13:14:50 -0500

    Journal App - Tests - environments

--- 06-journal-app/src/tests/store/journal/thunks.test.ts
+++ 06-journal-app/src/tests/store/journal/thunks.test.ts
@@ -1,4 +1,7 @@
+import { collection, deleteDoc, getDocs } from 'firebase/firestore/lite'
+
 import { startNewNote } from '../../../store'
+import { firebaseDB } from '../../../firebase/config'
 
 describe('Pruebas en Journal Thunks', () => {
 
@@ -20,6 +23,32 @@ describe('Pruebas en Journal Thunks', () => {
     })
 
     await startNewNote()(dispatch, getState, {})
+
+    const fulfilledAction = dispatch.mock.calls[1][0]    
+    
+    expect(fulfilledAction).toEqual({
+      meta: {
+        arg: undefined,
+        requestId: expect.any(String),
+        requestStatus: 'fulfilled'
+      },
+      payload: {
+        body: '',
+        date: expect.any(Number),
+        id: expect.any(String),
+        title: '',
+        imagesURLs: []
+      },
+      type: startNewNote.fulfilled.type
+    })
+
+    // Clear item from firestore
+    const collectionRef = collection(firebaseDB, `${uid}/journal/notes`)
+    const documents = await getDocs(collectionRef)
+
+    const deletePromises: Promise<void>[] = []
+    documents.forEach(doc => deletePromises.push(deleteDoc(doc.ref)))
+    await Promise.all(deletePromises)
 
   })
 
