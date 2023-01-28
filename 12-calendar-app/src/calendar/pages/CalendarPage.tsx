00ccdd498116388a54023b2c020ca33ed62e526f
ArianAngoma <arian.angoma@tecsup.edu.pe>
2023-01-28 18:15:04 -0500

    Calendar App - More configurations for the calendar

--- 12-calendar-app/src/calendar/pages/CalendarPage.tsx
+++ 12-calendar-app/src/calendar/pages/CalendarPage.tsx
@@ -1,39 +1,22 @@
-import { FC } from 'react'
+import { FC, CSSProperties } from 'react'
 
-import { Calendar, Event, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar, Event } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import { 
-  addHours,
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-} from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
+import { addHours } from 'date-fns'
 
 import { Navbar } from '../components'
+import { localizer } from '../helper'
 
-const locales = {
-  'en-US': enUS,
-}
+type CustomEvent = Event & { notes: string, bgColor: string, user: { _id: string, name: string } }
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
-
-// Todo: Type Event
-const events: (Event & any)[] = [
+const events: CustomEvent[] = [
   {
     title: 'All Day Event very long title',
     notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     start: new Date(),
     end: addHours(new Date(), 2),
-    bgColor: '#fafafa',
+    bgColor: 'blue',
     user: {
       _id: '123',
       name: 'Arian Angoma',
@@ -42,6 +25,21 @@ const events: (Event & any)[] = [
 ]
 
 const CalendarPage: FC = () => {
+
+  const eventStyleGetter = (event: CustomEvent, start: Date, end: Date, isSelected: boolean) => {
+    
+    const style: CSSProperties = {
+      backgroundColor: event.bgColor,
+      borderRadius: '0',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+
+  }
 
   return (
     <>
@@ -54,6 +52,7 @@ const CalendarPage: FC = () => {
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
+        eventPropGetter={eventStyleGetter}
       />
 
     </>
